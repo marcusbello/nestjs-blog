d437455db758db208e1d462f85789f43e6a6195c
Marcuses <marcusthetechguru@gmail.com>
2023-02-09 22:33:45 -0800

    Added blog resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { BlogModule } from './blog/blog.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Blog } from './blog/entities/blog.entity';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'goer_blog',
+      entities: [Blog],
+      synchronize: true,
+    }),
+    UsersModule,
+    BlogModule,
+  ],
 })
 export class AppModule {}
